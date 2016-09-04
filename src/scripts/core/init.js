25ea3b30bdd3df35ff1f16b64c1011b1d6a913b4
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-09-04 20:04:47 +0200

    feat(core): use circle geometry for character

--- src/scripts/core/init.js
+++ src/scripts/core/init.js
@@ -6,13 +6,13 @@ export default function init() {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
   camera.position.z = 1000;
 
-  const geometry = new THREE.BoxGeometry(200, 200, 200);
-  const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+  const geometry = new THREE.CircleGeometry(20, 32);
+  const material = new THREE.MeshBasicMaterial({color: 0xff0000});
 
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
