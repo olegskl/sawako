a5a42dea0c425fd7c41d401a76811b77cc54cca4
Oleg Sklyanchuk (oregu) <sklyanchuk@gmail.com>
2016-09-05 23:39:04 +0200

    feat(core): crisp movement

--- src/scripts/core/move.js
+++ src/scripts/core/move.js
@@ -1,22 +1,22 @@
 export default function move(vec, keys) {
 
+  vec.multiplyScalar(0.7);
+
   if (keys.ArrowRight) {
-    vec.setX(vec.x + 0.4);
+    vec.setX(vec.x + 0.5);
   }
 
   if (keys.ArrowLeft) {
-    vec.setX(vec.x - 0.4);
+    vec.setX(vec.x - 0.5);
   }
 
   if (keys.ArrowUp) {
-    vec.setY(vec.y + 0.4);
+    vec.setY(vec.y + 0.5);
   }
 
   if (keys.ArrowDown) {
-    vec.setY(vec.y - 0.4);
+    vec.setY(vec.y - 0.5);
   }
-
-  vec.multiplyScalar(0.8);
 
   if (vec.x > -0.1 && vec.x < 0.1) {
     vec.setX(0);
